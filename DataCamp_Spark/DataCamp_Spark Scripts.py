73f1d30894fcdd1e687947dcd054ea6b731b1a0d
DWang <66761776+Remember63@users.noreply.github.com>
2020-11-28 17:45:06 -0600

    Add files via upload

--- DataCamp_Spark/DataCamp_Spark Scripts.py
+++ DataCamp_Spark/DataCamp_Spark Scripts.py
@@ -86,5 +86,118 @@ file_path = "/usr/local/share/datasets/airports.csv"
 # Read in the airports data
 airports = spark.read.csv(file_path,header = True)
 
-# Show the data
-airports.show()
+
+# Notice that in the first case, we pass a string to .filter(). In SQL,
+# we would write this filtering task as SELECT * FROM flights WHERE air_time > 120.
+# Spark's .filter() can accept any expression that could go in the WHEREclause of a SQL query
+
+# Filter flights by passing a string
+long_flights1 = flights.filter("distance > 1000")
+
+# Filter flights by passing a column of boolean values
+long_flights2 = flights.filter(flights.distance > 1000)
+
+# Select the first set of columns
+selected1 = flights.select('tailnum','origin','dest')
+
+# Select the second set of columns
+temp = flights.select(flights.origin, flights.dest, flights.carrier)
+
+# Select the columns "origin", "dest", "tailnum", and avg_speed (without quotes!).
+
+# Define avg_speed
+avg_speed = (flights.distance/(flights.air_time/60)).alias("avg_speed")
+
+# Select the correct columns
+speed1 = flights.select("origin", "dest", "tailnum", avg_speed)
+
+# Create the same table using a SQL expression
+speed2 = flights.selectExpr("origin", "dest", "tailnum", "distance/(air_time/60) as avg_speed")
+
+# This creates a GroupedData object (so you can use the .min() method),
+# then finds the minimum value in col, and returns it as a DataFrame.
+
+# Find the shortest flight from PDX in terms of distance
+flights.filter("origin == 'PDX'").groupBy().min('distance').show()
+
+# Find the longest flight from SEA in terms of air time
+flights.filter(flights.origin == 'SEA').groupBy().max('air_time').show()
+
+# Grouping and Aggregating II
+# In addition to the GroupedData methods you've already seen, there is also the .agg() method.
+# This method lets you pass an aggregate column expression
+# that uses any of the aggregate functions from the pyspark.sql.functions submodule.
+
+# Import pyspark.sql.functions as F
+import pyspark.sql.functions as F
+
+# Group by month and dest
+by_month_dest = flights.groupBy("month","dest")
+
+# Average departure delay by month and destination
+by_month_dest.avg("dep_delay").show()
+
+# Standard deviation of departure delay
+by_month_dest.agg(F.stddev("dep_delay")).show()
+
+# Joining
+
+# Rename the faa column
+airports = airports.withColumnRenamed('faa','dest')
+
+# Join the DataFrames
+flights_with_airports = flights.join(airports, on = 'dest', how = 'leftouter')
+
+# Machine Learning Pipelines
+# In the next two chapters you'll step through every stage of the machine learning pipeline,
+# from data intake to model evaluation. Let's get to it!
+#
+# At the core of the pyspark.ml module are the Transformer and Estimator classes.
+# Almost every other class in the module behaves similarly to these two basic classes.
+#
+# Transformer classes have a .transform() method that takes a DataFrame and returns a new DataFrame;
+# usually the original one with a new column appended.
+# For example, you might use the class Bucketizer to create discrete bins
+# from a continuous feature or the class PCA to reduce the dimensionality
+# of your dataset using principal component analysis.
+#
+# Estimator classes all implement a .fit() method.
+# These methods also take a DataFrame, but instead of returning another DataFrame they return a model object.
+# This can be something like a StringIndexerModel for including categorical data saved as strings in your models,
+# or a RandomForestModel that uses the random forest algorithm for classification or regression.
+
+# Fortunately, PySpark has functions for handling this built into the pyspark.ml.features submodule.
+#
+# The first step to encoding your categorical feature is to create a StringIndexer.
+# Members of this class are Estimators that take a DataFrame with a column of strings and map each unique string to a number.
+# Then, the Estimator returns a Transformer that takes a DataFrame, attaches the mapping to it as metadata,
+# and returns a new DataFrame with a numeric column corresponding to the string column.
+#
+# The second step is to encode this numeric column as a one-hot vector using a OneHotEncoder.
+# This works exactly the same way as the StringIndexer by creating an Estimator and then a Transformer.
+# The end result is a column that encodes your categorical feature as a vector that's suitable for machine learning routines!
+#
+# This may seem complicated, but don't worry! All you have to remember is that
+# you need to create a StringIndexer and a OneHotEncoder, and the Pipeline will take care of the rest.
+
+# Create a StringIndexer
+carr_indexer = StringIndexer(inputCol = 'carrier', outputCol = 'carrier_index')
+
+# Create a OneHotEncoder
+carr_encoder = OneHotEncoder(inputCol = 'carrier_index', outputCol = 'carrier_fact')
+
+# Make a VectorAssembler
+vec_assembler = VectorAssembler(inputCols=['month', 'air_time', 'carrier_fact', 'dest_fact', 'plane_age'], outputCol='features')
+
+# Create the pipeline
+# Import Pipeline
+from pyspark.ml import Pipeline
+
+# Make the pipeline
+flights_pipe = Pipeline(stages=[dest_indexer, dest_encoder, carr_indexer, carr_encoder, vec_assembler])
+
+# Fit and transform the data
+piped_data = flights_pipe.fit(model_data).transform(model_data)
+
+# Split the data into training and test sets
+training, test = piped_data.randomSplit([0.6,0.4])
